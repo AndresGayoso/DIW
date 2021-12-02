448d95e14337b43bd8cb949f387fb3b56f9ec7a7
AndresGS <agayososegui@iessonferrer.net>
2021-12-02 19:35:23 +0100

    diw

--- CSS/ActividadAudio/index.js
+++ CSS/ActividadAudio/index.js
@@ -14,21 +14,3 @@ myAudio.onpause = function() {
 
 var play = function(event){   event.srcElement.classList.toggle("playing")
 }
-
-const updateProgress = () =>{
-	if (player.currentTime >0){
-		const barra = document.getElementById('progress')
-		barra.value = (player.currentTime / player.duration) * 100
-		
-		var duracionSegundos= player.duration.toFixed(0);
-		dura=secondsToString(duracionSegundos);
-		var actualSegundos = player.currentTime.toFixed(0)
-		actual=secondsToString(actualSegundos);
-		
-		duracion= actual +' / '+ dura
-		document.getElementById('timer').innerText=duracion 
-	}
-	if (player.ended){
-		nextMusic();//Reproducir la siguiente pista
-	} 
-}
